0ae6055f88ae0736af92c21e9100c3c45a17ccde
Usuário <usuario@MacBook-Pro-de-Usuario.local>
2018-01-09 13:47:31 -0200

    Atualizando variaveis

--- reunioes/progressos/Iniciante/Variáveis/variaveis.php
+++ reunioes/progressos/Iniciante/Variáveis/variaveis.php
@@ -25,51 +25,75 @@
     </div>
     <div class="container">
         <div class="row">
-            <?php
-                // defini uma variável sem valor
-                $nome = "Variável nome criada sem valor"; 
-                echo $nome;
+				<?php
+					// defini uma variável sem valor
+					$nome = "Variável nome criada sem valor"; 
+					echo $nome;
+					echo "</br>";
+				
 
-                echo "</br>";
-             
-
-                // variáveis do tipo numérica
-                $idade = "25"; 
-                $altura = "1.82";
-                echo "Minha idade é " . $idade . " e minha altura é " . $altura;
-
-                echo "</br>";
-
-                // Varivel String
-                $nomeCompleto = "Julio Rodrigues";
-                echo "meu nome completo é: " . $nomeCompleto;
-
-                echo "</br>";
-                // Array 
-                $carro = ['Celta', 'Gol', 'Fusion'];
-                echo "Carro escolhido do array: " . $carro[2]; // Imprime Fusion
-
-								echo "</br>";
-
-								// Variável do tipo Objeto 
-								class ExemploObjeto
-								{
-										function comprimentar()
-										{
-												echo "Olá Pessoal, criei uma objeto";
-										}
-								}
-								
-								$varObj = new ExemploObjeto;
-								$varObj->comprimentar();
-
-								echo "</br>";
-
-								
+					// variáveis do tipo numérica
+					$idade = "25"; 
+					$altura = "1.82";
+					echo "Minha idade é " . $idade . " e minha altura é " . $altura;
+					echo "</br>";
 
 
-            ?>
-        </div>
+					// Varivel String
+					$nomeCompleto = "Julio Rodrigues";
+					echo "meu nome completo é: " . $nomeCompleto;
+					echo "</br>";
+
+
+					// Array 
+					$carro = ['Celta', 'Gol', 'Fusion'];
+					echo "Carro escolhido do array: " . $carro[2]; // Imprime Fusion
+					echo $carro[3] = "Voyage";
+					echo "</br>";
+					var_dump($carro);
+					echo "</br>";
+
+
+					// Variável do tipo Objeto 
+					class ExemploObjeto {
+						function comprimentar()
+						{
+								echo "Olá Pessoal, criei um objeto";
+						}
+					}
+					$varObj = new ExemploObjeto;
+					$varObj->comprimentar();
+					echo "</br>";
+					
+
+					// Resources ou Recursos 
+					/*
+							Resource na verdade é um handler, ou uma função manipuladora, como por exemplo um mysqli_connect, manipulador de arquivos ou algo do tipo.
+					*/
+
+
+					// Como o próprio nome sugere, o tipo misto (mixed, em inglês) indica que uma variável pode receber valores de vários tipos.
+					$nome = "Julio";
+					echo gettype($nome); // o gettype pega qualquer valor o retorna o tipo do mesmo
+					echo "</br>";
+
+
+					// Call Back é a referencia de uma função.
+					function ExibirMensagem() {
+							echo "Seja bem vindo.";
+					}
+					call_user_func("ExibirMensagem"); // a funcao call_user_func recebe como parametro o nome da funcao e o executa
+					echo "</br>";
+
+					
+					// NULL - é utilizado para saber se uma variável possuí um valor
+					$nome = "Aqui tem valor";
+					echo var_dump($nome);
+					echo "</br>";
+					$sem_valor;
+					echo var_dump($sem_valor);
+				?>
+			</div>
     </div>
 </body>
 </html>
